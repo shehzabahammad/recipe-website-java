17f2868754c96204da396cb43a648afad6394dc5
Shehzab Ahammad <shehzabahammad@gmail.com>
2023-02-15 15:04:53 +0530

    initial commit

---
+++ src/main/java/com/shehzab/recipewebsite/RecipeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.shehzab.recipewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeWebsiteApplication.class, args);
+	}
+
+}
